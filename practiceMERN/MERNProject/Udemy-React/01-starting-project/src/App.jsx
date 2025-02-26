14fced2b1d33228d9584a88556c0849b544c4a64
stacksrahul <139710388+stacksrahul@users.noreply.github.com>
2025-02-26 19:01:27 +0000

    Add more component to the main body

--- practiceMERN/MERNProject/Udemy-React/01-starting-project/src/App.jsx
+++ practiceMERN/MERNProject/Udemy-React/01-starting-project/src/App.jsx
@@ -1,7 +1,20 @@
-const reactDescription = ['Fundamental','Crucial','Core']
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png"
+import propsImg from "./assets/config.png"
+const reactDescription = ['Fundamental','Crucial','Core'];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+function Components(props) {
+  return(
+    <li>
+      <img src={props.img} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
 }
 
 function Header() {
@@ -9,7 +22,7 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
@@ -25,6 +38,24 @@ function App() {
       <Header />
       <main>
         <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <Components 
+              title="Components"
+              description="The core UI building block"
+              img={componentsImg}
+            />
+            <Components 
+              title="Props"
+              description=""
+              img={propsImg}
+            />
+            <Components />
+            <Components />
+          </ul>
+        </section>
+        
       </main>
     </div>
   );
